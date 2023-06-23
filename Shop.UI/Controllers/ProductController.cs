320c67abf559a3545403557e9edefd0a8976e63d
Elmeddin Mirzeyev <118839615+Elmeddin996@users.noreply.github.com>
2023-06-24 01:41:38 +0400

    View Added

--- Shop.UI/Controllers/ProductController.cs
+++ Shop.UI/Controllers/ProductController.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using RestSharp;
+using Shop.UI.Models;
 
 namespace Shop.UI.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        HttpClient _client;
+        public ProductController()
         {
-            return View();
+            _client = new HttpClient();
+        }
+        public async Task<IActionResult> Index()
+        {
+            //var token = Request.Cookies["auth_token"];
+            //_client.DefaultRequestHeaders.Add(HeaderNames.Authorization, token);
+
+            using (var response = await _client.GetAsync("https://localhost:7233/api/Products"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<List<ProductGetAllResponse>>(content);
+
+                    return View(data);
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized || response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                    return RedirectToAction("login", "account");
+            }
+            return View("error");
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var token = Request.Cookies["auth_token"];
+            _client.DefaultRequestHeaders.Add(HeaderNames.Authorization, token);
+
+            using (var response = await _client.GetAsync("https://localhost:7233/api/Brands/all"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<List<BrandGetAllResponse>>(content);
+
+                    ViewBag.Brands = data;
+                    return View();
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized || response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                    return RedirectToAction("login", "account");
+            }
+            return View("error");
         }
     }
 }
